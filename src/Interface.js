af1c86fa59105133be0fbc5c7214dbf296195e58
nishadbhujbal <nishadbhujbal128@gmail.com>
2024-01-29 20:42:17 +0530

    Fixed key press events

--- src/Interface.js
+++ src/Interface.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
 const Interface = () => {
@@ -27,6 +27,7 @@ const Interface = () => {
 
   const [clickValue, setClickValue] = useState("");
   const [display, setDisplay] = useState([""]);
+  const [onKeyPressDisplay, setOnKeyPressDisplay] = useState([""]);
 
   const getValue = (value) => {
     setClickValue(value);
@@ -39,8 +40,14 @@ const Interface = () => {
         return [...previousState.slice(0, -1), newLastEntry];
       });
     } else if (value === "=") {
-      const x = eval(display[0]);
-      setDisplay(x);
+      setDisplay((previousState) => {
+        try {
+          const result = eval(previousState[0]);
+          return [result.toString()];
+        } catch (error) {
+          return ["Error"];
+        }
+      });
     } else {
       setDisplay((previousState) => {
         const lastEntry = previousState[previousState.length - 1];
@@ -49,12 +56,41 @@ const Interface = () => {
     }
   };
 
+  const detectKeyUp = (e) => {
+    setClickValue(e.key);
+    if (e.key === "Backspace") {
+      setOnKeyPressDisplay((previousState) => {
+        const lastEntry = previousState[previousState.length - 1];
+        const newLastEntry = lastEntry.slice(0, -1);
+        return [...previousState.slice(0, -1), newLastEntry];
+      });
+    } else if (e.key === "Enter") {
+      setOnKeyPressDisplay((previousState) => {
+        try {
+          const result = eval(previousState[0]);
+          return [result.toString()];
+        } catch (error) {
+          return ["Error"];
+        }
+      });
+    } else {
+      setOnKeyPressDisplay((previousState) => {
+        const lastEntry = previousState[previousState.length - 1];
+        return [...previousState.slice(0, -1), lastEntry + e.key];
+      });
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keyup", detectKeyUp, true);
+  }, []);
+
   return (
     <div className="App h-screen flex justify-center items-center">
       <div className="h-[450px] w-[320px] bg-[#272829] rounded-2xl p-6">
-        <div className="bg-[#BFCFE7] h-20 rounded-t-2xl mb-2 flex items-end">
+        <div className="bg-[#BFCFE7] h-20 rounded-t-2xl mb-2 flex items-end overflow-hidden">
           <p className="display w-[100%] flex justify-end font-semibold text-4xl pr-2 text-[#161718] tracking-2">
-            {display}
+            {display != "" ? display : onKeyPressDisplay}
           </p>
         </div>
         <div className="bg-[#272829] h-80 rounded-b-2xl grid grid-cols-4 grid-row gap-2">
